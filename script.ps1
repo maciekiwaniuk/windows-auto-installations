ce19d7391bae6fe5028f8407b2e1ce2b2ee86110
macieeek <iwaniukmaciej.kontakt@gmail.com>
2022-08-07 20:54:14 +0200

    Add function that installs apps listed in json file

--- script.ps1
+++ script.ps1
@@ -8,5 +8,39 @@ function installAndUpdateChocolatey {
     choco upgrade chocolatey;
 }
 
-installAndUpdateChocolatey;
+# Installs apps listed in json file
+function installApplicationsListedInFile {
+    $fileName = 'apps_to_install.json';
 
+    $applicationsToInstall = (Get-Content .\$fileName) | ConvertFrom-Json
+
+    foreach ($app in $applicationsToInstall) {
+        # check if there is already installed specific app in the system
+        $callback = choco find $app;
+        $didntFindInstalledPackage = ($callback.Split(' ')[-3] -eq 0);
+    
+        # system doesn't have installed certain app
+        if ($didntFindInstalledPackage) {
+            # check if chocolatey has exact app available to install
+            $callback = choco search $app;
+            $foundCertainAppAvailableToInstall = ($callback.Split(' ')[-3] -gt 0);
+
+            if ($foundCertainAppAvailableToInstall) {
+                choco install $app;
+
+            } else {
+                Write-Output "Chocolatey didn't find application '$app'";
+            }
+
+        } else {
+            # there is already installed certain app in the system
+            Write-Output "Requested application '$app' has been found already installed in the system.";
+        }
+    }
+    
+    
+}
+
+
+# installAndUpdateChocolatey;
+installApplicationsListedInFile;
